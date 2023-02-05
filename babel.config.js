883c16b28a7bababb1189a1f072c3a0ff1a8eb8b
mason369 <1960638223@qq.com>
2023-02-05 12:00:04 +0800

    优化代码，使代码更简洁

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
-// 这是项目发布阶段需要用到的babel插件
-const prodPlugins = [];
-// 如果是发布阶段,处于生产环境,就向prodPlugins数组里添加一个插件
-if (process.env.NODE_ENV === "production") {
-	prodPlugins.push("transform-remove-console");
-}
+const prodPlugins =
+	process.env.NODE_ENV === "production" ? ["transform-remove-console"] : []; // 判断当前环境是否为生产环境，如果是，则将transform-remove-console加入prodPlugins数组中
 module.exports = {
+	// 导出配置，使用@vue/cli-plugin-babel/preset作为预设，并将prodPlugins数组中的插件作为插件
 	presets: ["@vue/cli-plugin-babel/preset"],
-	plugins: [...prodPlugins] // 这里...展开运算符，把上面定义的prodPlugins数组里面的每一项展开添加到这个plugins数组里面
+	plugins: [...prodPlugins],
 };
--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,7 @@
-// vue.config.js
-
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-	transpileDependencies: true
-});
 
-module.exports = {
+module.exports = defineConfig({
+	transpileDependencies: true, // 设置依赖文件是否被转译
 	publicPath: "/Vue-project-template/",
 	lintOnSave: false,
-	// publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
-
-	//代理服务器
-	// devServer: {
-	// 	proxy: {
-	// 		"/api": {
-	// 			target      : "http://gmall-h5-api.atguigu.cn", //代理转发路径
-	// 			pathRewrites: { "^/api": "" }, //匹配字符串
-	// 			ws          : true, //支持websocket
-	// 			changeOrigin: true //跨域
-	// 		}
-	// 	}
-	// }
-};
+});
